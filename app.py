0eacd8d188200fcfdea1ea8638587d564650c091
thewitchesroad <escartinalex8@gmail.com>
2026-04-15 16:32:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -153,6 +153,7 @@ def logout():
 # 🧠 PREDICT
 # =========================================================
 def predict_page():
+    st.subheader("Fake News Detection System")
 
     news = st.text_area("Enter News")
     language = st.selectbox("Language", ["English", "Bisaya"])
