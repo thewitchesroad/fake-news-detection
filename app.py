3860f9fb155d55754cf2502318478738332299ae
thewitchesroad <escartinalex8@gmail.com>
2026-04-27 09:58:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,6 @@ import pandas as pd
 from supabase import create_client, Client
 from streamlit_cookies_manager import EncryptedCookieManager
 
-st.markdown("""
-    <style>
-        [data-testid="stHeader"] {display: none;}
-        [data-testid="stToolbar"] {display: none;}
-        [data-testid="stDecoration"] {display: none;}
-    </style>
-""", unsafe_allow_html=True)
 
 # ---------------- SUPABASE ----------------
 SUPABASE_URL = "https://dpvzvywjxsmsjcmbbgif.supabase.co"
